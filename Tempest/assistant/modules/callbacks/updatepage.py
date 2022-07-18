2cabe0a4670f4db7ed0e05f10fbf6d516cafde49
TempestNetwork <101667772+TempestNetwork@users.noreply.github.com>
2022-07-18 15:29:40 +0530

    Create updatepage.py

---
+++ Tempest/assistant/modules/callbacks/updatepage.py
@@ -0,0 +1,23 @@
+""""
+This file creates pages for updating the userbot to latet versions
+"""
+
+from pyrogram import filters
+
+from pyrogram.types import (
+    CallbackQuery
+)
+
+from Tempest.userbot.client import app
+
+
+
+
+
+@app.bot.on_callback_query(filters.regex("update-tab"))
+@app.alert_user
+async def _update_callback(_, cb: CallbackQuery):
+    await cb.answer(
+            text="This feature is not implemented yet.",
+            show_alert=True
+        )
