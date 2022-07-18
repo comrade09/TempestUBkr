8933802584398bbfe57c8d4d9f88f4bc5ec0c3ff
TempestNetwork <101667772+TempestNetwork@users.noreply.github.com>
2022-07-18 15:26:32 +0530

    Create settingspage.py

---
+++ Tempest/assistant/modules/callbacks/settingspage.py
@@ -0,0 +1,43 @@
+"""
+This file creates pages for settings in help menu.
+"""
+
+from pyrogram import filters
+
+from pyrogram.types import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    CallbackQuery,
+)
+from Tempest.userbot.client import app
+
+
+
+
+
+@app.bot.on_callback_query(filters.regex("settings-tab"))
+@app.alert_user
+async def _settings(_, cb: CallbackQuery):
+    await cb.edit_message_text(
+        text=app.settings_tab_string(),
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Restart bot", callback_data="restart-tab",
+                    ),
+                ],
+                [
+                    InlineKeyboardButton(
+                        "Shutdown bot", callback_data="shutdown-tab",
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "Update bot", callback_data="update-tab",
+                    )
+                ],
+                app.BuildKeyboard((["Home", "close-tab"], ["Back", "home-tab"])),
+            ]
+        ),
+    )
