590225c94517aee3c4025afb321ffc0bdeb10496
TempestNetwork <101667772+TempestNetwork@users.noreply.github.com>
2022-07-18 15:18:34 +0530

    Create extrapage.py

---
+++ Tempest/assistant/modules/callbacks/extrapage.py
@@ -0,0 +1,44 @@
+"""
+This file creates extra page tab menu in helpdex
+"""
+
+from pyrogram import filters
+
+from pyrogram.types import (
+	InlineKeyboardButton,
+	InlineKeyboardMarkup,
+	CallbackQuery,
+)
+
+from Tempest.userbot.client import app
+
+
+
+
+
+@app.bot.on_callback_query(filters.regex("extra-tab"))
+@app.alert_user
+async def _extra(_, cb: CallbackQuery):
+    await cb.edit_message_text(
+        text=app.extra_tab_string(),
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="• Public commands •",
+                        callback_data="ubpublic-commands-tab"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        text="Home",
+                        callback_data="close-tab"
+                    ),
+                    InlineKeyboardButton(
+                        text="Back",
+                        callback_data="home-tab"
+                    )
+                ]
+            ]
+        )
+    )
