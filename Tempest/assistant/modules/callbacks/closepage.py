5b7184a0fe7e3c9a2049369dac90e06e931a4d7d
TempestNetwork <101667772+TempestNetwork@users.noreply.github.com>
2022-07-18 15:17:41 +0530

    Create closepage.py

---
+++ Tempest/assistant/modules/callbacks/closepage.py
@@ -0,0 +1,44 @@
+"""
+This file is for closed page inline help menu.
+"""
+
+from pyrogram import filters
+
+from pyrogram.types import (
+    InputMediaPhoto,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    CallbackQuery
+)
+
+from Tempest.userbot.client import app
+
+
+
+
+
+@app.bot.on_callback_query(filters.regex("close-tab"))
+@app.alert_user
+async def _close(_, cb: CallbackQuery):
+    await cb.edit_message_media(
+        media=InputMediaPhoto(
+            media="Tempest/core/resources/images/tempest-vertical.png", 
+            caption=app.close_tab_string()
+        ),
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="Open",
+                        callback_data="home-tab"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        text="Delete",
+                        callback_data="delete-tab"
+                    )
+                ]
+            ]
+        )
+    )
