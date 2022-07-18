a259f789ab75fa24170f6722bb6b68afb6c311b5
TempestNetwork <101667772+TempestNetwork@users.noreply.github.com>
2022-07-18 15:19:29 +0530

    Create homepage.py

---
+++ Tempest/assistant/modules/callbacks/homepage.py
@@ -0,0 +1,41 @@
+""".
+This file creates home page of helpmenu.
+"""
+
+from pyrogram import filters
+
+from pyrogram.types import (
+    InlineKeyboardMarkup,
+    InputMediaPhoto,
+    CallbackQuery,
+)
+
+from Tempest.userbot.client import app
+
+
+
+
+
+@app.bot.on_callback_query(filters.regex("home-tab"))
+@app.alert_user
+async def _start(_, cb: CallbackQuery):
+    await cb.edit_message_media(
+        media=InputMediaPhoto(media=app.BotPic(), caption=app.home_tab_string()),
+        reply_markup=InlineKeyboardMarkup([
+                app.BuildKeyboard(
+                    (
+                        ["• Settings •", "settings-tab"],
+                        ["• Plugins •", "plugins-tab"]
+                    )
+                ),
+                app.BuildKeyboard(
+                    (
+                        ["• Extra •", "extra-tab"],
+                        ["• Stats •", "stats-tab"]
+                    )
+                ),
+                app.BuildKeyboard(([["About", "about-tab"]])),
+                app.BuildKeyboard(([["Close", "close-tab"]]))
+        ]
+        ),
+    )
